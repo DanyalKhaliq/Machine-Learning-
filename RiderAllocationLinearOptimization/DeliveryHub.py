55f297835b88d71f3b7a2d108eb9f708432dd372
Danyal Khaliq <33676888+DanyalKhaliq@users.noreply.github.com>
2018-12-12 12:16:20 +0500

    Add files via upload

--- RiderAllocationLinearOptimization/DeliveryHub.py
+++ RiderAllocationLinearOptimization/DeliveryHub.py
@@ -16,20 +16,31 @@ class DeliveryHub:
     def CalculateOptimizedSolution(self):
         
              
+        #Set Orders Number like that so the Optimal solution can be derived . a little manipulation of data 
+        if self.MaxRiders > 0:
+            minCapacity = self.RiderCapacity
+        elif self.MaxCabs > 0:
+            minCapacity = self.CabCapacity
+        else:
+            return "No Result Possible with this Data !"
+        
+        remainder = self.TotalHubOrders % minCapacity
+        remainder = minCapacity - remainder if remainder > 0 else 0
+        
         prob = LpProblem("The Whiskas Problem",LpMinimize)
         
         x1=LpVariable("Rider",0,None,cat=pulp.LpInteger)
         x2=LpVariable("Cab",0,None,cat=pulp.LpInteger)
         
         # defines the constraints
-        prob += self.RiderCapacity*x1+self.CabCapacity*x2 == self.TotalHubOrders
+        prob += self.RiderCapacity*x1+self.CabCapacity*x2 == self.TotalHubOrders + remainder
         prob += x1+x2 <= self.MaxRiders + self.MaxCabs
         prob += x1<= self.MaxRiders
         prob += x2<= self.MaxCabs
         prob += x1>=0
         prob += x2>=0
         
-        prob += 2.5*self.DistanceToCover*x1 + 10*self.DistanceToCover*x2, "Total Cost of fuel / km"
+        prob += self.CostRider*self.DistanceToCover*x1 + self.CostCab*self.DistanceToCover*x2, "Total Cost of fuel / km"
         
         
         prob.solve()
@@ -45,8 +56,8 @@ class DeliveryHub:
         
         dictObj = {"Hub Name" : self.HubName , 
                    'Riders' : prob.variables()[1].varValue,'Cabs' : prob.variables()[0].varValue,
-                   'Total Rider & Cab Cost' : str(value(prob.objective)), 
+                   'Total Cost' : str(value(prob.objective)), 
                    'Solution Status' : LpStatus[prob.status],
-                   "Total Deliverable Capacity by selected Combination" : str(prob.variables()[1].varValue*self.RiderCapacity + prob.variables()[0].varValue*self.CabCapacity)}
+                   "Un-Used Capacity" : str(remainder)}
                    
         return dictObj
