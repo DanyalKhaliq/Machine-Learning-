a56a051f6151180809d70e7b94123c7fc67eeba9
Danyal Khaliq <33676888+DanyalKhaliq@users.noreply.github.com>
2020-02-25 15:48:05 +0500

    Add files via upload

---
+++ DamagerSeverityDetector/Configuration.py
@@ -0,0 +1,28 @@
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+
+class CustomConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "damage"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + toy
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 85% confidence
+    DETECTION_MIN_CONFIDENCE = 0.90
+
+class InferenceConfig(CustomConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
